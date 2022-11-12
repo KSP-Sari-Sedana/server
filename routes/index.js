7c3f617fe03466f703e28fccf163e3d89ed9a4f5
gusde <69630915+slickeel@users.noreply.github.com>
2022-11-12 20:47:33 +0800

    feat: setup openapi

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const swagger = require("swagger-ui-express");
+const swaggerDocument = require("../openapi.json");
 
 router.get("/", (req, res) => {
   res.json({ message: "server running" });
 });
+
+router.use("/api/openapi", swagger.serve);
+router.get("/api/openapi", swagger.setup(swaggerDocument));
 
 module.exports = router;
