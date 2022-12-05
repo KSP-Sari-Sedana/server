4723d8fe543a6e61f3366c45a4ff93e885869a73
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-05 13:02:00 +0800

    fix: try catch for next middleware

--- app/utils/tryCatch.js
+++ app/utils/tryCatch.js
@@ -1,6 +1,6 @@
 const tryCatch = (controller) => async (req, res, next) => {
   try {
-    await controller(req, res);
+    await controller(req, res, next);
   } catch (error) {
     return next(error);
   }
