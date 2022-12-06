adcb1c35a7d40cb9eb8714587f9a1969eadefb90
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-06 20:27:09 +0800

    feat: find a product by id

--- app/repositories/productRepository.js
+++ app/repositories/productRepository.js
@@ -6,4 +6,10 @@ async function findAll() {
   return result;
 }
 
-export default { findAll };
+async function findById(id) {
+  const query = `SELECT nama, foto, deskripsi, TRUNCATE(bunga, 2) AS bunga, status, tipe, setoran, tenor, angsuran FROM produk WHERE id = ?`;
+  const [result] = await dbPool.execute(query, [id]);
+  return result;
+}
+
+export default { findAll, findById };
