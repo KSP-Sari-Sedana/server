bee0c01a7c57c0680c4f95a871aecc4144160184
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-06 20:22:17 +0800

    fix: get products field

--- app/repositories/productRepository.js
+++ app/repositories/productRepository.js
@@ -1,7 +1,7 @@
 import dbPool from "../../config/database.js";
 
 async function findAll() {
-  const query = `SELECT nama, deskripsi, TRUNCATE(bunga, 2) AS bunga, status, tipe, setoran FROM produk WHERE status = 'aktif'`;
+  const query = `SELECT nama, foto, deskripsi, TRUNCATE(bunga, 2) AS bunga, tipe, setoran, tenor, angsuran, status FROM produk WHERE status = 'aktif'`;
   const [result] = await dbPool.execute(query);
   return result;
 }
