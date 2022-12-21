d92dcdf5819b9796ee503a869a10b0c3a2498d59
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 21:20:37 +0800

    feat: database port

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 import mysql from "mysql2";
 import("dotenv/config");
 
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
 
 const dbPool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
+  port: DB_PORT,
 });
 
 export default dbPool.promise();
