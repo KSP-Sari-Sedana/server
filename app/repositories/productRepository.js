23dd9b8bda2da5137f57d50ff8627eeba5612a97
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-25 18:02:07 +0800

    fix: select id product

--- app/repositories/productRepository.js
+++ app/repositories/productRepository.js
@@ -1,13 +1,13 @@
 import dbPool from "../../config/database.js";
 
 async function findAll() {
-  const query = `SELECT nama, foto, deskripsi, TRUNCATE(bunga, 2) AS bunga, tipe, setoran, tenor, angsuran, status FROM produk WHERE status = 'aktif'`;
+  const query = `SELECT id, nama, foto, deskripsi, TRUNCATE(bunga, 2) AS bunga, tipe, setoran, tenor, angsuran, status FROM produk WHERE status = 'aktif'`;
   const [result] = await dbPool.execute(query);
   return result;
 }
 
 async function findById(id) {
-  const query = `SELECT nama, foto, deskripsi, TRUNCATE(bunga, 2) AS bunga, status, tipe, setoran, tenor, angsuran FROM produk WHERE id = ?`;
+  const query = `SELECT id, nama, foto, deskripsi, TRUNCATE(bunga, 2) AS bunga, status, tipe, setoran, tenor, angsuran FROM produk WHERE id = ?`;
   const [result] = await dbPool.execute(query, [id]);
   return result[0];
 }
