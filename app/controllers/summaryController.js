8f63996be0ca8da8713b29df8ff97f60d54a68e6
gusde <69630915+slickeel@users.noreply.github.com>
2023-01-21 08:02:57 +0800

    fix: undefined user summary

--- app/controllers/summaryController.js
+++ app/controllers/summaryController.js
@@ -1,27 +1,24 @@
 import summaryRepository from "../repositories/summaryRepository.js";
-import { APIError, ReqError } from "../helpers/appError.js";
 import { APISuccess } from "../helpers/response.js";
-import errorCode from "../constants/errorCode.js";
 
 async function getSummary(req, res) {
   const { id } = req.user;
 
-  const saving = await summaryRepository.findSavingBalance(id);
-  if (!saving) throw new ReqError(errorCode.RESOURCE_NOT_FOUND, "Summary tidak ditemukan", 404);
-
+  let saving = await summaryRepository.findSavingBalance(id);
   const { loanTotal } = await summaryRepository.findLoanTotal(id);
-  if (!loanTotal) throw new ReqError(errorCode.RESOURCE_NOT_FOUND, "Summary tidak ditemukan", 404);
-
   const { totalLoanPayment } = await summaryRepository.findTotalLoanPayment(id);
-  if (!totalLoanPayment) throw new ReqError(errorCode.RESOURCE_NOT_FOUND, "Summary tidak ditemukan", 404);
+
+  saving = {
+    balance: saving.balance || 0,
+  };
 
   const loan = {
-    loanTotal,
-    totalLoanPayment,
+    loanTotal: loanTotal || 0,
+    totalLoanPayment: totalLoanPayment || 0,
     loanBalance: loanTotal - totalLoanPayment,
   };
 
-  res.status(200).json(APISuccess("Summary berhasil ditemukan", { saving, loan }));
+  res.status(200).json(APISuccess("Summary berhasil didapatkan", { saving, loan }));
 }
 
 export default { getSummary };
