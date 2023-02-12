ee3460a888fc25111694ecf1485b6dd052fd8b19
gusde <69630915+slickeel@users.noreply.github.com>
2023-02-12 08:24:43 +0800

    fix: request size limit

--- config/express.js
+++ config/express.js
@@ -2,20 +2,21 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import cors from "cors";
+import bodyParser from "body-parser";
 import "dotenv/config";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import routes from "../app/routes/indexRoute.js";
 import errorHandler from "../app/middlewares/errorHandler.js";
-import { ReqError } from "../app/helpers/appError.js";
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+var limit = "1mb";
+app.use(bodyParser.json({ limit: limit }));
+app.use(bodyParser.urlencoded({ limit: limit, extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(dirname(fileURLToPath(import.meta.url)), "../public")));
 
